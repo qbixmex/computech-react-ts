13e636cceef7e992fdab5008a918a57a4da7a87a
Daniel González Briseño <qbixmex@gmail.com>
2023-07-12 11:00:04 -0700

    added-eslint-prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,20 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': 'warn',
     'react-hooks/exhaustive-deps': 'off',
   },
-}
+};
